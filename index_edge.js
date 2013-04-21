87b366cee4e2d72efc13521dd808c1e40e286bd7
Johnathon Larsen <johnathon.larsen.1@student.prcc.edu>
2013-04-21 15:38:41 -0500

    Frog Rotation Animation
    Created the Frog Rotation Animation by moving him left, right, right,
    and left by 8 degrees each time using the rotation transform tool.

--- index_edge.js
+++ index_edge.js
@@ -28,6 +28,9 @@ var symbols = {
       },
    states: {
       "Base State": {
+         "${_content} > div:nth-child(1) > div:nth-child(1) > img:nth-child(1)": [
+            ["transform", "rotateZ", '0deg']
+         ],
          "body > div:nth-child(1)": [
             ["transform", "translateX", '0px'],
             ["transform", "translateY", '-100px']
@@ -64,6 +67,7 @@ var symbols = {
          duration: 2500,
          autoPlay: true,
          timeline: [
+            { id: "eid50", tween: [ "style", "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)", "height", '354px', { fromValue: '354px'}], position: 2500, duration: 0 },
             { id: "eid59", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)", "translateY", '0px', { fromValue: '-101px'}], position: 0, duration: 1000 },
             { id: "eid22", tween: [ "style", "${_content} > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)", "opacity", '1', { fromValue: '0'}], position: 0, duration: 1000 },
             { id: "eid41", tween: [ "transform", "${_content} > div:nth-child(2) > div:nth-child(1) > h2:nth-child(1)", "translateX", '-60px', { fromValue: '-967px'}], position: 0, duration: 2000 },
@@ -77,7 +81,10 @@ var symbols = {
             { id: "eid51", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)", "translateX", '0px', { fromValue: '0px'}], position: 0, duration: 0 },
             { id: "eid61", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)", "translateX", '0px', { fromValue: '0px'}], position: 1000, duration: 0 },
             { id: "eid35", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > h1:nth-child(1)", "scaleX", '1', { fromValue: '0.3619'}], position: 0, duration: 1000 },
-            { id: "eid50", tween: [ "style", "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)", "height", '354px', { fromValue: '354px'}], position: 2500, duration: 0 },
+            { id: "eid77", tween: [ "transform", "${_content} > div:nth-child(1) > div:nth-child(1) > img:nth-child(1)", "rotateZ", '-8deg', { fromValue: '0deg'}], position: 0, duration: 250 },
+            { id: "eid78", tween: [ "transform", "${_content} > div:nth-child(1) > div:nth-child(1) > img:nth-child(1)", "rotateZ", '0deg', { fromValue: '-8deg'}], position: 250, duration: 250 },
+            { id: "eid79", tween: [ "transform", "${_content} > div:nth-child(1) > div:nth-child(1) > img:nth-child(1)", "rotateZ", '8deg', { fromValue: '0deg'}], position: 500, duration: 250 },
+            { id: "eid80", tween: [ "transform", "${_content} > div:nth-child(1) > div:nth-child(1) > img:nth-child(1)", "rotateZ", '0deg', { fromValue: '8deg'}], position: 750, duration: 250 },
             { id: "eid74", tween: [ "transform", "body > div:nth-child(1)", "translateY", '-4px', { fromValue: '-100px'}], position: 0, duration: 800 },
             { id: "eid48", tween: [ "style", "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)", "width", '1011px', { fromValue: '1011px'}], position: 2500, duration: 0 },
             { id: "eid36", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > h1:nth-child(1)", "scaleY", '1', { fromValue: '0.3619'}], position: 0, duration: 1000 },
