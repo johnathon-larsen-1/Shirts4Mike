f18514f02a5b8241974c14cdc2366a8bc1f238aa
Johnathon Larsen <johnathon.larsen.1@student.prcc.edu>
2013-04-21 15:37:05 -0500

    Nav Bar Animation
    Created the Nav Bar Animation by moving it up offscreen then sliding it
    down.

--- index_edge.js
+++ index_edge.js
@@ -28,26 +28,32 @@ var symbols = {
       },
    states: {
       "Base State": {
+         "body > div:nth-child(1)": [
+            ["transform", "translateX", '0px'],
+            ["transform", "translateY", '-100px']
+         ],
+         "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)": [
+            ["transform", "translateX", '0px'],
+            ["transform", "translateY", '-101px']
+         ],
+         "${_content} > div:nth-child(2)": [
+            ["style", "height", '378px'],
+            ["transform", "translateX", '-10px'],
+            ["transform", "translateY", '24px']
+         ],
          "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)": [
             ["style", "height", '354px'],
             ["style", "opacity", '0'],
             ["transform", "translateY", '2px'],
             ["style", "width", '1011px']
          ],
-         "${_content} > div:nth-child(2)": [
-            ["style", "height", '378px']
+         "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)": [
+            ["transform", "translateX", '0px'],
+            ["transform", "translateY", '-101px']
          ],
          "${_content} > div:nth-child(2) > div:nth-child(1) > h2:nth-child(1)": [
             ["transform", "translateX", '-967px'],
             ["transform", "translateY", '0px']
-         ],
-         "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)": [
-            ["transform", "translateX", '0px'],
-            ["transform", "translateY", '-101px']
-         ],
-         "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)": [
-            ["transform", "translateX", '0px'],
-            ["transform", "translateY", '-101px']
          ]
       }
    },
@@ -61,15 +67,18 @@ var symbols = {
             { id: "eid59", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)", "translateY", '0px', { fromValue: '-101px'}], position: 0, duration: 1000 },
             { id: "eid22", tween: [ "style", "${_content} > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)", "opacity", '1', { fromValue: '0'}], position: 0, duration: 1000 },
             { id: "eid41", tween: [ "transform", "${_content} > div:nth-child(2) > div:nth-child(1) > h2:nth-child(1)", "translateX", '-60px', { fromValue: '-967px'}], position: 0, duration: 2000 },
+            { id: "eid49", tween: [ "transform", "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)", "translateY", '2px', { fromValue: '2px'}], position: 2500, duration: 0 },
             { id: "eid47", tween: [ "style", "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)", "opacity", '1', { fromValue: '0'}], position: 0, duration: 2500 },
             { id: "eid60", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)", "translateY", '0px', { fromValue: '-101px'}], position: 0, duration: 1000 },
+            { id: "eid72", tween: [ "transform", "body > div:nth-child(1)", "translateX", '0px', { fromValue: '0px'}], position: 0, duration: 0 },
+            { id: "eid75", tween: [ "transform", "body > div:nth-child(1)", "translateX", '0px', { fromValue: '0px'}], position: 800, duration: 0 },
             { id: "eid52", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)", "translateX", '0px', { fromValue: '0px'}], position: 0, duration: 0 },
             { id: "eid62", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)", "translateX", '0px', { fromValue: '0px'}], position: 1000, duration: 0 },
             { id: "eid51", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)", "translateX", '0px', { fromValue: '0px'}], position: 0, duration: 0 },
             { id: "eid61", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)", "translateX", '0px', { fromValue: '0px'}], position: 1000, duration: 0 },
             { id: "eid35", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > h1:nth-child(1)", "scaleX", '1', { fromValue: '0.3619'}], position: 0, duration: 1000 },
             { id: "eid50", tween: [ "style", "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)", "height", '354px', { fromValue: '354px'}], position: 2500, duration: 0 },
-            { id: "eid49", tween: [ "transform", "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)", "translateY", '2px', { fromValue: '2px'}], position: 2500, duration: 0 },
+            { id: "eid74", tween: [ "transform", "body > div:nth-child(1)", "translateY", '-4px', { fromValue: '-100px'}], position: 0, duration: 800 },
             { id: "eid48", tween: [ "style", "${_content} > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2)", "width", '1011px', { fromValue: '1011px'}], position: 2500, duration: 0 },
             { id: "eid36", tween: [ "transform", "body > div:nth-child(1) > div:nth-child(1) > h1:nth-child(1)", "scaleY", '1', { fromValue: '0.3619'}], position: 0, duration: 1000 },
             { id: "eid40", tween: [ "transform", "${_content} > div:nth-child(2) > div:nth-child(1) > h2:nth-child(1)", "translateY", '0px', { fromValue: '0px'}], position: 0, duration: 0 }         ]
